6af6046570040a1d814aa3368f8061a596afa0c1
Stepan Shi <ste-shi@mail.ru>
2022-04-04 16:37:45 +0300

    routing

--- src/init.js
+++ src/init.js
@@ -17,10 +17,7 @@ export default function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
+              <Link to="/login">Login</Link>
             </li>
           </ul>
         </nav>
@@ -28,11 +25,8 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/">
             <Home />
@@ -47,10 +41,6 @@ function Home() {
   return <h2>Home</h2>;
 }
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
+function Login() {
+  return <h2>LLogin</h2>;
 }
